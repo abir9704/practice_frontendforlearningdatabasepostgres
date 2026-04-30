ad5b869e404ccf97002c86c9e6c5a4de7272107c
abir781 <hasan.abir3176@gmail.com>
2026-04-30 08:34:28 +0600

    kkkssl

--- app/singleproduct/[id]/Order.tsx
+++ app/singleproduct/[id]/Order.tsx
@@ -37,6 +37,14 @@ const Order = () => {
               body: JSON.stringify(data),
         })
 
+          fetch("http://localhost:8000/singleproductsinfo",{
+            method:"POST",
+            headers:{
+                'Content-Type': 'application/json',
+            },
+              body: JSON.stringify(data),
+        })
+
         fetch("http://localhost:8000/username",{
             method:"POST",
             headers:{
